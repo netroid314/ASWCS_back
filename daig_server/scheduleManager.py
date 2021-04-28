9011e37ec7ea994bf607812684cc089add8f4b18
dyddnrdl3@naver.com <dyddnrdl3@naver.com>
2021-04-28 23:30:45 +0900

    add schedule manager for users. further implementations required

---
+++ daig_server/scheduleManager.py
@@ -0,0 +1,12 @@
+
+
+class scheduleManager:
+
+    # user list consists of pair between user identificator(id or serial number) and his state
+    # which is like 'learning' and 'standby'
+    user_list = {'dummy_user_no':'dummy_state'}
+
+    def __init__(self):
+        user_list = {}
+
+        
